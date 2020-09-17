a7afbc272d3c9941b2818f293c6facffcb7c2d57
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-09-17 17:40:19 +0200

    done

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,24 +1,36 @@
-// // import "./utils.js"
-// import subtract, {square, add} from "./utils.js"
-
-// console.log("app.js is running")
-// console.log(square(4))
-// console.log(add(100,23))
-// console.log(subtract(100,81))
-// console.log(anythingIWant(100,81))
-
-// import react from "validator"
-// import isSenior, {isAdult, canDrink} from "./person.js"
-
-import React from "react"
+ import React from "react"
 import ReactDOM from "react-dom"
 
-const template = React.createElement("p", {}, "testing 123")
-ReactDOM.render(template,document.getElementById("app"))
+class Header extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>This is from Header!</p>
+            </div>
+        ) 
+    }
+}
 
-// console.log(isAdult(17))
-// console.log(isAdult(21))
-// console.log(isSenior(64))
+class Action extends React.Component{
+    render(){
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        ) 
+    }
+}
+
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header />
+    </div>
+)
+
+ReactDOM.render(jsx,document.getElementById("app"))
+
 
 
 
