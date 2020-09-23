2ebc0c743be75e6a6f94e92af4d19d1b69f12fd7
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-09-23 13:23:30 +0200

    done

---
+++ expensify-app/src/playground/app.js
@@ -0,0 +1,29 @@
+class Header extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>This is from Header!</p>
+            </div>
+        ) 
+    }
+}
+
+class Action extends React.Component{
+    render(){
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        ) 
+    }
+}
+
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header />
+    </div>
+)
+
+ReactDOM.render(jsx,document.getElementById("app"))
