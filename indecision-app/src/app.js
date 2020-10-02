0394a3ecfead28d102ee6171963fe5ba47caf047
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-10-02 15:33:09 +0200

    done

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,35 +1,10 @@
- import React from "react"
+import React from "react"
 import ReactDOM from "react-dom"
+import IndecisionApp from "./components/IndecisionApp"
+import "normalize.css/normalize.css"
+import "./styles/styles.css"
 
-class Header extends React.Component{
-    render(){
-        return (
-            <div>
-                <p>This is from Header!</p>
-            </div>
-        ) 
-    }
-}
-
-class Action extends React.Component{
-    render(){
-        return (
-            <div>
-                <button>What should I do?</button>
-            </div>
-        ) 
-    }
-}
-
-
-const jsx = (
-    <div>
-        <h1>Title</h1>
-        <Header />
-    </div>
-)
-
-ReactDOM.render(jsx,document.getElementById("app"))
+ReactDOM.render(<IndecisionApp/>,document.getElementById("app"))
 
 
 
